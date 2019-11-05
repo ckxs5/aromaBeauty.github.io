e02b8cbd42408c95cbdb74d70d76b46cd682ebe4
Kaixuan Chen <32051171+ckxs5@users.noreply.github.com>
2019-11-05 16:27:42 -0500

    Draft complete finished

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	var mymap = L.map('mapid').setView([40.762247,-73.9869336], 18);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', 
+	{
+        id: 'mapbox.streets'
+    }).addTo(mymap);
+
 	$('.menu-icon').click(function(){
 		$('ul').toggleClass('active');
 	})
