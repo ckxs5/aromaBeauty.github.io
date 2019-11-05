4f03b9bc81fafc04fbf579fc3ecd9a1544d691ab
Kaixuan Chen <32051171+ckxs5@users.noreply.github.com>
2019-11-05 16:51:22 -0500

    location changed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var mymap = L.map('mapid').setView([40.762247,-73.9869336], 18);
+	var mymap = L.map('mapid').setView([40.7290963,-74.0089746], 16);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', 
 	{
